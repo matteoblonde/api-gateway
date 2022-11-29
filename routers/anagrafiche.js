761cddad16cdf21e49f7da4819cc38fcd925b88a
matteoballarini <matteo.ballarini@icloud.com>
2022-11-29 21:04:28 +0100

    Added call to Anagrafiche v0.1

--- routers/anagrafiche.js
+++ routers/anagrafiche.js
@@ -28,22 +28,6 @@ async function waitFor(tester) {
   }
 }
 
-/***
- *
- VERSIONE TYPESCRIPT
-
- export default async function will<T, K = any>(tester: Promise<T>): Promise<[ K | null, T ]> {
-  try {
-    const result = await tester;
-    return [ null, result ];
-  }
-  catch (e: any) {
-    return [ e, null as unknown as T ];
-  }
-}
-
- */
-
 
 /**
  *
